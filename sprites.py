b7e6fce35c9449f814da0e0400aace891e03677a
pchai <prempreychhay@gmail.com>
2025-03-07 07:46:12 -0800

    lesson 2

--- sprites.py
+++ sprites.py
@@ -19,6 +19,13 @@ class Player(pygame.sprite.Sprite):
         self.width = TILESIZE
         self.height = TILESIZE
         
+        #these are temp var of change in movement during a loop
+        self.x_change = 0
+        self.y_change = 0
+        
+        #default
+        self.facing = 'down'
+        
         self.image = pygame.Surface([self.width, self.height])
         self.image.fill(RED)
         
@@ -28,4 +35,25 @@ class Player(pygame.sprite.Sprite):
         self.rect.y = self.y
         
     def update(self):
-        pass
+        self.movement()
+        
+        self.rect.x += self.x_change
+        self.rect.y += self.y_change
+        
+        self.x_change = 0
+        self.y_change = 0
+    
+    def movement(self):
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_LEFT] or keys[pygame.K_a]:
+            self.x_change -= PLAYER_SPEED
+            self.facing = 'left'
+        if keys[pygame.K_RIGHT] or keys[pygame.K_d]:
+            self.x_change += PLAYER_SPEED
+            self.facing = 'right'
+        if keys[pygame.K_UP] or keys[pygame.K_w]:
+            self.y_change -= PLAYER_SPEED
+            self.facing = 'up'
+        if keys[pygame.K_DOWN] or keys[pygame.K_s]:
+            self.y_change += PLAYER_SPEED
+            self.facing = 'down'
